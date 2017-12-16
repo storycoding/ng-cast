a7601a581ad3bd87acff140803f4dc303af3d9fa
storycoding <youknownuno@gmail.com>
2017-12-15 16:31:04 -0800

    Replaced template for video data example

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,30 @@
 angular.module('video-player')
 
 .component('app', {
-  templateUrl: 'src/templates/app.html',
+  controller: function() {
+    this.data = exampleVideoData;
+  }, 
+
+  template: `<div id="app container">
+    <nav class="navbar">
+      <div class="col-md-6 col-md-offset-3">
+        <search class="search-bar">
+      </div>
+    </nav>
+
+      <div class="col-md-7">
+        <video-player class="video-player">
+      </div>
+        
+      <div class="col-md-5">
+        <video-list class="video-list">
+      </div>
+    <div>
+</div>`
   
 });
 //not sure about ctrl.name on line 4
+
+// <video-player><h5><em>videoPlayer</em> component goes here</h5></video-player>
+// <search><h5><em>search</em> component goes here</h5></search>
+// <video-list><h5><em>videoList</em> component goes here</h5></video-list>
--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,5 +1,44 @@
 angular.module('video-player')
 
 .component('videoPlayer', {
-  templateUrl: 'src/templates/videoPlayer.html'
+  controller: function() {
+    this.data = exampleVideoData;
+    this.videoId = 'https://www.youtube.com/embed/' + this.data[0].id.videoId;
+  },
+  template: `
+    <div class="video-player">
+      <div class="embed-responsive embed-responsive-16by9">
+        <iframe class="embed-responsive-item" src={{$ctrl.videoId}} allowFullScreen></iframe>
+      </div>
+    <div class="video-player-details">
+      <h3>Video Title</h3>
+      <div>Video Description</div>
+    </div>
+  </div>
+  `
 });
+
+
+// var VideoPlayer = (props) => {
+  
+//     //var videoPlayer = this.props.videoPlayer;
+//     //var iframe = this.props.iframe;
+
+//   var thumbnail = props.video.snippet.thumbnails.default.url;
+//   var videoId = 'https://www.youtube.com/embed/' + props.video.id.videoId;
+//   var description = props.video.snippet.description;
+//   var title = props.video.snippet.title;
+
+//   return (
+//     <div className="video-player">
+//       <div className="embed-responsive embed-responsive-16by9">
+//         <iframe className="embed-responsive-item" src={videoId} allowFullScreen></iframe>
+//       </div>
+//       <div className="video-player-details">
+//         <h3>{title}</h3>
+//         <div>{description}</div>
+//       </div>
+//     </div>
+//   );
+// };
+
